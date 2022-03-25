0b3734d98ae7e37317fa166cace015733fc3a3ea
matfreitasc <matfreitasc@gmail.com>
2022-03-25 18:25:31 -0400

    success on creating new user and login in

--- back-end/app.js
+++ back-end/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const sauceRoutes = require("./routes/sauces");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -33,5 +34,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
